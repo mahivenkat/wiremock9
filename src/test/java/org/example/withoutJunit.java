7a730a263c3521d668f2480999cd0ed6bd3ad633
Venkat Kamani <kamanivenkat@gmail.com>
2022-10-02 22:37:30 +0530

    proxy routing

--- src/test/java/org/example/withoutJunit.java
+++ src/test/java/org/example/withoutJunit.java
@@ -2,15 +2,17 @@ package org.example;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static wiremock.org.eclipse.jetty.webapp.MetaDataComplete.True;
 
 public class withoutJunit {
     public static void main(String[] args) {
         WireMock.configureFor("localhost", 8080);
 
-        WireMockServer wm = new WireMockServer(wireMockConfig().port(8080)); //No-args constructor will start on port 8080, no HTTPS
+        WireMockServer wm = new WireMockServer(wireMockConfig()); //No-args constructor will start on port 8080, no HTTPS
         wm.start();
         wm.stubFor(get("/app1")
                 .withHost(equalTo("localhost"))
@@ -21,8 +23,23 @@ public class withoutJunit {
         wm.stubFor(get("/app3")
 //                .withHost(equalTo("localhost"))
                 .willReturn(ok("app3 reply")));
+//        wm.stubFor(proxyAllTo("https://jsonplaceholder.typicode.com/posts").withName("/api4").);
 
-// Do some stuff
+            wm.stubFor(get(urlMatching("/posts"))
+                    .willReturn(aResponse().proxiedFrom("https://jsonplaceholder.typicode.com")));
+
+
+
+        stubFor(get(urlEqualTo("/posts"))
+                .willReturn(aResponse()
+                        .withBodyFile("resp.json")));
+
+        stubFor(get("/json")
+                .willReturn(okJson("{ \"message\": \"Hello\" }")));
+
+
+
+        // Do some stuff
 
 //        WireMock.reset();
 
